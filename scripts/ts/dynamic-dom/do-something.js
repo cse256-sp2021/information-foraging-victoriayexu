98dde713cbe65dfbbecf7299cc92baf56c21e7da
Victoria Xu <vyxu@wustl.edu>
2021-02-19 20:20:18 -0600

    Test initial commit

--- scripts/ts/dynamic-dom/do-something.js
+++ scripts/ts/dynamic-dom/do-something.js
@@ -1,3 +1,16 @@
 export function doSomething() {
   console.log("Put some code in here!");
+  window.onscroll = stickyFunction;
+
+  var container = document.getElementById("main-menu-container");
+  var sticky = container.offsetTop;
+
+  function stickyFunction() {
+    console.log("sticky");
+    if (window.pageYOffset > sticky) {
+      container.classList.add("sticky");
+    } else {
+      container.classList.remove("sticky");
+    }
+  }
 }
